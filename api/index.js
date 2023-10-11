574be41c86d3f1c3d5071adce935a77faa5d9d6a
Ebie <adewumiebenezer7@gmail.com>
2023-10-11 11:38:28 +0100

    model done

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("Connected to Mongodb"))
+.catch(err => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+app.listen("5000", () => {
+    console.log(`Server is running on port 5000`);
+})
