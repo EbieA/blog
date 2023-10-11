6424f8e4d2e844809a0faf13c527cfeac7429906
Ebie <adewumiebenezer7@gmail.com>
2023-10-11 15:54:01 +0100

    almost

--- api/index.js
+++ api/index.js
@@ -2,17 +2,24 @@ const express = require('express')
 const app = express();
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/post")
 
 dotenv.config();
+app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(console.log("Connected to Mongodb"))
 .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-  });
+app.use("/api/auth", authRoute)  
+app.use("/api/user", userRoute)  
+app.use("/api/posts", postRoute)  
 
 app.listen("5000", () => {
     console.log(`Server is running on port 5000`);
